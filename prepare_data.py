975891a6f72bcc36c6367563f1f33f069f1f28c8
sultaaaane <mohamadalaoui77@gmail.com>
2025-11-28 12:47:24 +0100

    storing the data into vector database and embeddit in chroma db (a vector database)

--- prepare_data.py
+++ prepare_data.py
@@ -1,7 +1,34 @@
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+import os
+import shutil
+from langchain_chroma import Chroma
 from langchain_community.document_loaders import DirectoryLoader
+from langchain_huggingface.embeddings import HuggingFaceEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 BOOKS_PATH = "./books"
+CHROMA_PATH = "./db/chroma_langchain_db"
+
+
+def get_embeddings(embedding_function):
+
+    embeddings = embedding_function
+    return embeddings
+
+
+def store_vectore_db(docs):
+    if os.path.exists(CHROMA_PATH):
+        shutil.rmtree(CHROMA_PATH)
+    embeddings = get_embeddings(
+        HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+    )
+    db = Chroma(
+        collection_name="Books",
+        embedding_function=embeddings,
+        persist_directory=CHROMA_PATH,
+    )
+    db.add_documents(documents=docs)
+    result = db.similarity_search("what is the context")
+    print(result)
 
 
 def load_documents():
@@ -10,16 +37,18 @@ def load_documents():
     return documents
 
 
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000,
-    chunk_overlap=200,
-    add_start_index=True,
-)
+def split_text(documents):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200,
+        add_start_index=True,
+    )
+    return text_splitter.split_documents(documents)
 
 
 docs = load_documents()
 if not docs:
     print(f"No documents found in {BOOKS_PATH}")
 else:
-    all_splits = text_splitter.split_documents(docs)
-    print(all_splits[10])
+    all_splits = split_text(docs)
+    store_vectore_db(docs)
